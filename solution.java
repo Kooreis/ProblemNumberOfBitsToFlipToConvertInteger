7ed622237dfcdf4935379b253edc6333cee48227
Kooreis <kooreis.official@gmail.com>
2024-12-22 06:13:14 -0600

    In the second update, the program prompts the user to enter two integers and stores them in 'num1' and 'num2'.

--- solution.java
+++ solution.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first integer:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second integer:");
+        int num2 = scanner.nextInt();
     }
 }
